4987761a92977b1d29addb163aa6152c8aafaa41
nganltt9 <nganlee169@gmail.com>
2025-01-11 21:32:27 +0700

    update testcase 03 hover & click

--- selenium/webdriver/Topic_17_Action_P1.java
+++ selenium/webdriver/Topic_17_Action_P1.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -20,7 +21,10 @@ public class Topic_17_Action_P1 {
 
     @BeforeClass
     public void initialBrowser() {
+        //Selenium sẽ init  browser lên với config mặc định
+        //Tham số để nhận diện đang chạy browser bằng automation test
         driver = new EdgeDriver();
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         action = new Actions(driver);
     }
@@ -35,5 +39,21 @@ public class Topic_17_Action_P1 {
         Assert.assertEquals(driver.findElement(By.cssSelector("div.ui-tooltip-content")).getText(), "We ask for your age only for statistical purposes.");
 
     }
+
+    @Test
+    public void TC_02_Fahasa() throws InterruptedException {
+        driver.get("https://www.fahasa.com/");
+
+        Thread.sleep(5000);
+
+        action.moveToElement(driver.findElement(By.xpath("//span[@class='icon_menu']")))
+                .pause(Duration.ofSeconds(3)).perform();
+        action.moveToElement(driver.findElement(By.xpath("//span[text()='VPP - Dụng Cụ Học Sinh']")))
+                .pause(Duration.ofSeconds(3)).perform();
+        action.click(driver.findElement(By.xpath("//div[contains(@class,'fhs_menu_content')]//a[text()='Gọt Bút Chì']"))).perform();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("ol.breadcrumb strong")).getText(), "GỌT BÚT CHÌ");
+    }
+
 
 }
