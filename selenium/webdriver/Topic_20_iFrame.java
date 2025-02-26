692492c2059b28ac4c1c4c2ee93acebb66129cbc
nganle <nganlee169@gmail.com>
2025-02-26 07:57:45 +0700

    Topic 20: iframe

--- selenium/webdriver/Topic_20_iFrame.java
+++ selenium/webdriver/Topic_20_iFrame.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -30,16 +32,69 @@ public class Topic_20_iFrame {
     public void TC_01_Map() {
         driver.get("https://www.embedgooglemap.net/");
 
-        // Switch vào iframe
+        // Switch vào iframe B
         driver.switchTo().frame(driver.findElement(By.cssSelector("div.gmap_canvas>iframe")));
 
         String addressName = driver.findElement(By.cssSelector("div.place-name")).getText();
         System.out.println(addressName);
+
+        // Switch vào iframe C
+        driver.switchTo().frame(driver.findElement(By.cssSelector("div#mapDiv>div>div>iframe")));
+
+        // Đang ở C
+        System.out.println(driver.getPageSource());
+
+        // Từ C quay lại B
+        driver.switchTo().parentFrame();
+
+        addressName = driver.findElement(By.cssSelector("div.place-name")).getText();
+        System.out.println(addressName);
+
+        // B quay lại A
+        driver.switchTo().defaultContent();
+
+        driver.findElement(By.cssSelector("input#s")).clear();
+        driver.findElement(By.cssSelector("input#s")).sendKeys("22 Le Loi, Ho Chi Minh");
+
     }
+
+    @Test
+    public void TC_02_FormSite() throws InterruptedException {
+        driver.get("https://www.formsite.com/templates/education/campus-safety-survey/");
+
+        driver.findElement(By.cssSelector("button.osano-cm-accept-all")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.cssSelector("div#imageTemplateContainer>img")).click();
+        Thread.sleep(3000);
+
+        driver.switchTo().frame(driver.findElement(By.cssSelector("div#formTemplateContainer>iframe")));
+
+        new Select(driver.findElement(By.xpath("//label[contains(text(),'Year')]/following-sibling::select"))).selectByVisibleText("Sophomore");
+        new Select(driver.findElement(By.xpath("//label[contains(text(),'Residence')]/following-sibling::select"))).selectByVisibleText("South Dorm");
+        driver.findElement(By.xpath("//label[contains(text(),'Male')]")).click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.cssSelector("input#FSsubmit")).click();
+
+        // Từ trong iframe switch ra ngoài chưa
+        driver.switchTo().defaultContent();
+
+        driver.findElement(By.cssSelector("nav[class='header header--desktop'] a.menu-item-login")).click();
+        Thread.sleep(3000);
+
+        driver.findElement(By.cssSelector("button#login")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#message-error")).getText(), "Username and password are both required.");
+
+    }
+
 
     @AfterClass
     public void cleanBrowser() {
-        driver.quit();
+
+        // driver.quit();
     }
 
 }
